8d6ac5e1b25fc92902795408f9ba9d0e41e823f6
julyL <849424162@qq.com>
2018-04-02 09:52:49 +0800

    添加lie源码

---
+++ lie/demo.js
@@ -0,0 +1,12 @@
+var Promise = require("./index");
+// var Promise = require("../simple-promise.js");
+new Promise(resolve => {
+  console.log(1);
+  resolve(3);
+  new Promise((re, rj) => {
+    re();
+  }).then(() => console.log(4));
+}).then(num => {
+  console.log(num);
+});
+console.log(2);
